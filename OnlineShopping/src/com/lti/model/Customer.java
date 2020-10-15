de353725b97956020a4968742bf6b12fd7ed9a02
DELL <DELL@DESKTOP-RJ7LTOA>
2020-10-15 15:49:48 +0530

    modified customer

--- OnlineShopping/src/com/lti/model/Customer.java
+++ OnlineShopping/src/com/lti/model/Customer.java
@@ -3,6 +3,13 @@ package com.lti.model;
 public class Customer {
 	int custId;
 	String custName;
+	int custCity;
+	public int getCustCity() {
+		return custCity;
+	}
+	public void setCustCity(int custCity) {
+		this.custCity = custCity;
+	}
 	public int getCustId() {
 		return custId;
 	}
