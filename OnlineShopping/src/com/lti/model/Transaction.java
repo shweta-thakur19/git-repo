fe35ddad0478c45cbab1b365ac8557a0a3af4516
DELL <DELL@DESKTOP-RJ7LTOA>
2020-10-16 09:35:39 +0530

    new change of transaction

---
+++ OnlineShopping/src/com/lti/model/Transaction.java
@@ -0,0 +1,19 @@
+package com.lti.model;
+
+public class Transaction {
+	int transactionId;
+	double amount;
+	public int getTransactionId() {
+		return transactionId;
+	}
+	public void setTransactionId(int transactionId) {
+		this.transactionId = transactionId;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+}
